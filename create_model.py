3f792a95f1c4a88385bf73799d718c936134457b
Spectral <contest_shad@spectral.tech>
2023-03-29 21:30:49 +0400

    data_by_days

--- create_model.py
+++ create_model.py
@@ -3,13 +3,23 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
-df = pd.read_feather('train_competition.feather')
-y_true = df['target']
 
-model = LinearRegression(fit_intercept=False)
-SANHOK_columns = [column for column in df.columns if column.startswith('SANHOK')]
-model.fit(df[SANHOK_columns], y_true)
-y_pred = model.predict(df[SANHOK_columns])
-print('r2_score = ', r2_score(y_true, y_pred))
+def main():
+    data_dir = 'data_by_days'
+    NUM_TRAIN_FILES = 128
 
-np.save('coef.npy', model.coef_)
+    df = pd.concat([pd.read_feather(f'{data_dir}/train_{i}.feather') for i in range(NUM_TRAIN_FILES)])
+
+    y_true = df['target']
+
+    model = LinearRegression(fit_intercept=False)
+    SANHOK_columns = [column for column in df.columns if column.startswith('SANHOK')]
+    model.fit(df[SANHOK_columns], y_true)
+    y_pred = model.predict(df[SANHOK_columns])
+    print('r2_score = ', r2_score(y_true, y_pred))
+
+    np.save('coef.npy', model.coef_)
+
+
+if __name__ == '__main__':
+    main()
--- pipeline.py
+++ pipeline.py
@@ -1,17 +1,43 @@
+import argparse
+
+import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
 
 from solution import get_predict
 
+NUM_TRAIN_FILES = 128
+NUM_TEST_FILES = 56
+NUM_PUBLIC_TEST_FILES = 28
+
 
 def main():
-    df = pd.read_feather('test_competition.feather')
-    X_columns = [column for column in df.columns if column != 'target']
-    X = df[X_columns]
-    y_true = df['target']
-    y_pred = get_predict(X)
-    score = r2_score(y_true, y_pred)
-    return score
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", default='public_test', type=str)
+    args = parser.parse_args()
+    mode = args.mode
+
+    assert mode in ['train', 'test', 'public_test']
+    name = 'train' if mode == 'train' else 'test'
+    if mode == 'train':
+        NUM_FILES = NUM_TRAIN_FILES
+    if mode == 'test':
+        NUM_FILES = NUM_TEST_FILES
+    if mode == 'public_test':
+        NUM_FILES = NUM_PUBLIC_TEST_FILES
+
+    data_dir = 'data_by_days'
+    y_trues = []
+    y_preds = []
+
+    for i in range(NUM_FILES):
+        df = pd.read_feather(f'{data_dir}/{name}_{i}.feather')
+        if len(df) > 0:
+            X_columns = [column for column in df.columns if column != 'target']
+            y_trues.append(df['target'].values)
+            y_preds.append(get_predict(df[X_columns]))
+
+    return r2_score(np.concatenate(y_trues), np.concatenate(y_preds))
 
 
 if __name__ == '__main__':
